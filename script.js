c57a7dc86acde1fa5dcadfd4ee42146515a64a1c
mekcherie <69826367+mekcherie@users.noreply.github.com>
2020-10-08 16:03:43 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
-// const display = document.querySelector('#display')
+const display = document.querySelector('.display')
 const billInput = document.querySelector('#bill')
 const tipInput = document.querySelector('#tip')
-
-
 const peopleInput = document.querySelector('#people')
+const totalInput = document.querySelector('.total')
 
-const text = document.querySelector("#tip_per")
-const text2= document.querySelector("#bill")
-const text3 = document.querySelector("#bill")
-const text4 = document.querySelector('#total')
 
-function calculateTip() {
-    const billValue = Number(billInput.value) 
-    const tipValue = tipInput.value
-    const tip = (billValue * tipValue / 100)
-    const tipAmount = (billValue * tipValue / 100)  / peopleInput.value
-    const tipBill = (tip + billValue)
-    display.innerHTML = tipAmount.toFixed(2)
-    text4.innerHTML = `$${tipBill.toFixed(2)}`
+var billAmount= 0
+var TipAmount= 0
+var PeopleAmount=1
+
+
+tipInput.addEventListener('change', handleTip)
+billInput.addEventListener('change', handleBill)
+peopleInput.addEventListener('change', handlePeople)
+
+function handleBill(e) {
+  billAmount = parseInt(e.target.value)
+   console.log("billvalue", billAmount)
+   display.innerHTML= (TipAmount / 100)
+   totalInput.innerHTML= (PeopleAmount*(billAmount+TipAmount*billAmount))
 }
 
-function per() {
-  if(peopleInput.value >= 2){
-    text.innerHTML = "Tip Per Person"
-    const billValue = billInput.value
-    let bill = billValue/peopleInput.value
-    text2.innerHTML = 'Bill per person'
-    text3.innerHTML = `$${(bill.toFixed(2))}`
-  }else {
-    text.innerHTML = "Your Tip"
-    text2.innerHTML = ''
-    text3.innerHTML = ''
-  }
+function handleTip(e) {
+  TipAmount = parseInt(e.target.value)
+  console.log("tipvalue", TipAmount)
+  display.innerHTML= (TipAmount / 100)
+  totalInput.innerHTML= (PeopleAmount*(billAmount+TipAmount*billAmount))
 }
-calculateTip();
-billInput.addEventListener ('input', update)
-tipInput.addEventListener ('input', update)
-peopleInput.addEventListener ('input', update)
+
+function handlePeople(e) {
+  peopleAmount = parseInt(e.target.value)
+  console.log("peoplevalue", peopleAmount)
+  display.innerHTML= (TipAmount / 100)
+  totalInput.innerHTML= (PeopleAmount*(billAmount+TipAmount*billAmount))
+}
